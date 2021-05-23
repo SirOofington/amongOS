17eb9ea2f781fee43464c3725cdaed9751a38bb4
Hunter Jensen <11895282+SirOofington@users.noreply.github.com>
2021-05-23 18:49:09 -0500

    added aOSutils

---
+++ aOSutils.lua
@@ -0,0 +1,80 @@
+home_theme = {
+    head_txt = colors.yellow,
+    head_bck = colors.gray,
+    ui_txt = colors.white,
+    ui_bck = colors.black,
+    sel_txt = colors.black,
+    sel_bck = colors.white
+}
+
+twt_theme = {
+    head_txt = colors.white,
+    head_bck = colors.cyan,
+    ui_txt = colors.black,
+    ui_bck = colors.white,
+    sel_txt = colors.cyan,
+    sel_bck = colors.white
+}
+
+head_txt = nil
+head_bck = nil
+ui_txt = nil
+ui_bck = nil
+sel_txt = nil
+sel_bck = nil
+
+function set_home_theme()
+    head_txt = home_theme.head_txt
+    head_bck = home_theme.head_bck
+    ui_txt = home_theme.ui_txt
+    ui_bck = home_theme.ui_bck
+    sel_txt = home_theme.sel_txt
+    sel_bck = home_theme.sel_bck
+end
+
+function set_twt_theme()
+    head_txt = twt_theme.head_txt
+    head_bck = twt_theme.head_bck
+    ui_txt = twt_theme.ui_txt
+    ui_bck = twt_theme.ui_bck
+    sel_txt = twt_theme.sel_txt
+    sel_bck = twt_theme.sel_bck
+end
+
+function set_head_colors()
+    term.setBackgroundColor(head_bck)
+    term.setTextColor(head_txt)
+end
+
+function set_ui_colors()
+    term.setBackgroundColor(ui_bck)
+    term.setTextColor(ui_txt)
+end
+
+function set_sel_colors()
+    term.setBackgroundColor(sel_bck)
+    term.setTextColor(sel_txt)
+end
+
+function draw_text(x,y,str)
+    term.setCursorPos(x,y)
+    term.write(str)
+    term.setCursorPos(0,0)
+end
+
+function draw_header()
+    paintutils.drawFilledBox(1,1,w,h,ui_bck)
+    for i=1,w do
+        paintutils.drawPixel(i,1,head_bck)
+        paintutils.drawPixel(i,h,head_bck)
+    end
+    
+    set_head_colors()
+    term.setCursorPos(1,1)
+    term.write(string.format("%s %s",os_name,version))
+
+    term.setCursorPos(w - string.len("ID: *****") + 1,1)
+    term.write(string.format("ID: %5d",os.getComputerID()))
+
+    term.setCursorPos(0,0)
+end
--- amongOS.lua
+++ amongOS.lua
@@ -1,4 +1,4 @@
--- amongOS
+ os.loadAPI("aOSutils")
 
 os.pullEvent = os.pullEventRaw
 
@@ -8,77 +8,11 @@ w, h = term.getSize()
 selected_app = 1
 apps = {}
 
-home_theme = {
-    head_txt = colors.yellow,
-    head_bck = colors.gray,
-    ui_txt = colors.white,
-    ui_bck = colors.black,
-    sel_txt = colors.black,
-    sel_bck = colors.white
-}
-
-twt_theme = {
-    head_txt = colors.white,
-    head_bck = colors.cyan,
-    ui_txt = colors.black,
-    ui_bck = colors.white,
-    sel_txt = colors.cyan,
-    sel_bck = colors.white
-}
-
-head_txt = nil
-head_bck = nil
-ui_txt = nil
-ui_bck = nil
-sel_txt = nil
-sel_bck = nil
-
-function set_home_colors()
-    head_txt = home_theme.head_txt
-    head_bck = home_theme.head_bck
-    ui_txt = home_theme.ui_txt
-    ui_bck = home_theme.ui_bck
-    sel_txt = home_theme.sel_txt
-    sel_bck = home_theme.sel_bck
-end
-
-function set_twt_colors()
-    head_txt = twt_theme.head_txt
-    head_bck = twt_theme.head_bck
-    ui_txt = twt_theme.ui_txt
-    ui_bck = twt_theme.ui_bck
-    sel_txt = twt_theme.sel_txt
-    sel_bck = twt_theme.sel_bck
-end
-
-function draw_header()
-    paintutils.drawFilledBox(1,1,w,h,ui_bck)
-    for i=1,w do
-        paintutils.drawPixel(i,1,head_bck)
-        paintutils.drawPixel(i,h,head_bck)
-    end
-    
-    term.setTextColor(head_txt)
-    term.setCursorPos(1,1)
-    term.write(string.format("%s %s",os_name,version))
-
-    term.setCursorPos(w - string.len("ID: *****") + 1,1)
-    term.write(string.format("ID: %5d",os.getComputerID()))
-
-    term.setBackgroundColor(ui_bck)
-    term.setCursorPos(0,0)
-end
-
 function draw_apps()
     for i=1,#apps do
-        term.setTextColor(ui_txt)
-        term.setBackgroundColor(ui_bck)
-        if i == selected_app then
-            term.setTextColor(sel_txt)
-            term.setBackgroundColor(sel_bck)
-        end
-        term.setCursorPos(3,i + 2)
-        term.write(apps[i]["name"])
+        aOSutils.set_ui_colors()
+        if i == selected_app then aOSutils.set_sel_colors() end
+        aOSutils.draw_text(3,i + 2,apps[i]["name"])
         paintutils.drawPixel(1,i+2,apps[i]["ico"])
     end
     term.setCursorPos(0,0)
@@ -107,37 +41,25 @@ function cycle_apps()
 end
 
 function app_exit()
-    term.setTextColor(ui_txt)
-    term.setBackgroundColor(ui_bck)
+    aOSutils.set_ui_colors()
     term.clear()
-    draw_header()
-    term.setCursorPos(1,3)
-    term.setCursorBlink(false)
-    term.write("Shutting down")
+    aOSutils.draw_header()
+    aOSutils.draw_text(1,3,"Shutting down")
     for i=1,3 do
         os.sleep(0.5)
-        term.write(".")
+        aOSutils.draw_text(string.len("Shutting down") + i,3,".")
     end
     os.sleep(0.5)
     os.shutdown()
 end
 
 function app_info_draw_interface()
-    term.setCursorPos(1,3)
-    term.setBackgroundColor(ui_bck)
-    term.setTextColor(ui_txt)
-    term.write(string.format("Phone ID: %05d",os.getComputerID()))
-    term.setCursorPos(1,4)
-    term.write("Owner: ")
-    term.setCursorPos(1,5)
-    term.write(string.format("Firmware Version: %s",version))
-    term.setCursorPos(1,7)
-    term.setBackgroundColor(sel_bck)
-    term.setTextColor(sel_txt)
-    term.write("Back")
-    term.setBackgroundColor(ui_bck)
-    term.setTextColor(ui_txt)
-    term.setCursorPos(0,0)
+    aOSutils.set_ui_colors()
+    aOSutils.draw_text(1,3,string.format("Phone ID: %05d",os.getComputerID()))
+    aOSutils.draw_text(1,4,"Owner: ")
+    aOSutils.draw_text(1,5,string.format("Firmware Version: %s",version))
+    aOSutils.set_sel_colors()
+    aOSutils.draw_text(1,7,"Back")
 end
 
 function app_info_exit()
@@ -149,7 +71,7 @@ end
 function app_info()
     while true do
         term.clear()
-        draw_header()
+        aOSutils.draw_header()
         app_info_draw_interface()
         if app_info_exit() then
             break
@@ -158,39 +80,39 @@ function app_info()
 end
 
 function app_update()
-    local website = http.get("https://raw.githubusercontent.com/SirOofington/amongOS/test/amongOS.lua")
-    if website then
-        term.setBackgroundColor(ui_bck)
-        term.clear()
-        draw_header()
-        
-        local txt = website.readAll()
-        website.close()
+    local file_list = {"amongOS.lua","aOSutils.lua"}
+    for i=1,#file_list
+        local filename = file_list[i]
+        local website = http.get("https://raw.githubusercontent.com/SirOofington/amongOS/test/"..filename)
+        if website then
+            term.clear()
+            draw_header()
+            aOSutils.set_ui_colors()
+            
+            local txt = website.readAll()
+            website.close()
 
-        local file = fs.open(os_name..".lua","w")
-        file.write(txt)
-        file.close()
-
-        term.setCursorPos(1,3)
-        term.setTextColor(ui_txt)
-        term.setCursorBlink(false)
-        term.write("Updating")
-        for i=1,3 do
-            os.sleep(0.5)
-            term.write(".")
+            local file = fs.open(filename,"w")
+            file.write(txt)
+            file.close()
         end
-        os.sleep(0.5)
-
-        shell.run("delete startup")
-        shell.run("copy "..os_name..".lua startup")
-        os.reboot()
     end
+    aOSutils.draw_text(1,3,"Updating")
+    for i=1,3 do
+        os.sleep(0.5)
+        aOSutils.draw_text(string.len("Updating") + i,3,".")
+    end
+    os.sleep(0.5)
+
+    shell.run("delete startup")
+    shell.run("copy "..os_name..".lua startup")
+    os.reboot()
 end
 
 function app_twitter()
     while true do
-        set_twt_colors()
-        draw_header()
+        aOSutils.set_twt_colors()
+        aOSutils.draw_header()
         os.sleep(5)
         break
     end
@@ -205,9 +127,9 @@ apps = {
 }
 
 while true do
-    set_home_colors()
     term.clear()
-    draw_header()
+    aOSutils.set_home_colors()
+    aOSutils.draw_header()
     draw_apps()
     cycle_apps()
 end
