9c4cd2d8472e6df3b1e5a38c33f61acb2ffba00a
Hunter Jensen <11895282+SirOofington@users.noreply.github.com>
2021-05-24 21:16:15 -0500

    Update twitter.lua

--- external/twitter.lua
+++ external/twitter.lua
@@ -5,14 +5,10 @@ function draw_main_menu()
     aOSutils.draw_text(2,3,"Twitter")
 end
 
-function run()
+while true do
     aOSutils.set_theme(aOSutils.twt_theme)
     aOSutils.draw_header()
     draw_main_menu()
     os.sleep(5)
     break
-end
-
-while true do
-    run()
 end
