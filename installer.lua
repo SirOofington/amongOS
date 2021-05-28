a97bcad492c1d1695f2fdfa4fb59eaca9cfc1576
NanoSpectr <32341706+NanoSpectr@users.noreply.github.com>
2021-05-28 02:38:06 -0500

    Added command line functionality
    Added functionality to pull by branch (default "main") and choose install directory (default "disk/")

--- installer.lua
+++ installer.lua
@@ -1,37 +1,49 @@
-local website = http.get("https://raw.githubusercontent.com/SirOofington/amongOS/test/filelist.txt")
+local args = {...}
+local branch, dir
+
+print("--amongOS installer--")
+
+if not (args[1] == nil) then
+    branch = tostring(args[1])
+else
+    branch = "main"
+end
+print("Pulling files from "..branch.." branch")
+
+if args[2] == nil then
+    dir = "disk/"
+elseif args[2] == "none" then
+    dir = ""
+else
+    dir = tostring(args[2]).."/"
+end
+print("Installing to directory \""..dir.."\"")
+
+local website = http.get("https://raw.githubusercontent.com/SirOofington/amongOS/"..branch.."/filelist.txt")
+
+local file_list = {}
 
 if website then
-    local txt = website.readAll()
+        while true do
+            local file = website.readLine()
+            if file == nil then
+                break
+            end
+            table.insert(file_list,file)
+        end
     website.close()
-
-    local file = fs.open("filelist.txt","w")
-    file.write(txt)
-    file.close()
 else
     return nil
 end
 
-local file = fs.open("filelist.txt","r")
-local file_list = {}
-
-while true do
-    local temp = file.readLine()
-    if temp == nil then
-        break
-    end
-    table.insert(file_list,temp)
-end
-
-file.close()
-
 for i=1,#file_list do
     local filename = file_list[i]
-    local website = http.get("https://raw.githubusercontent.com/SirOofington/amongOS/test/"..filename)
+    local website = http.get("https://raw.githubusercontent.com/SirOofington/amongOS/"..branch.."/"..filename)
     if website then
         local txt = website.readAll()
         website.close()
 
-        local file = fs.open("disk/"..filename,"w")
+        local file = fs.open(dir..filename,"w")
         file.write(txt)
         file.close()
     end
