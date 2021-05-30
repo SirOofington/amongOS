69190af1945aad8b41e3d5109efb2f9fd33e5033
Hunter Jensen <11895282+SirOofington@users.noreply.github.com>
2021-05-30 16:57:50 -0500

    Update twitter.lua

--- external/twitter.lua
+++ external/twitter.lua
@@ -64,11 +64,11 @@ function draw_send_tweet_menu()
     aOSutils.draw_wrapped_text(3,9,22,input_text)
     
     if selected == 2 then aOSutils.set_colors("sel") end
-    aOSutils.draw_text(2,18,buttons["send_tweet"][1])
+    aOSutils.draw_text(2,18,buttons["send_tweet"][1]["name"])
 
     aOSutils.set_colors("ui")
     if selected == 3 then aOSutils.set_colors("sel") end
-    aOSutils.draw_text(aOSutils.w - #buttons["send_tweet"][2] - 1,18,buttons["send_tweet"][3])
+    aOSutils.draw_text(aOSutils.w - #buttons["send_tweet"][2]["name"] - 1,18,buttons["send_tweet"][2]["name"])
 end
 
 function send_tweet_menu()
